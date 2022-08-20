ec4ab5e5b019d1cd26cffe98e9773282cae11c31
Nathaniel <nfcopier@gmail.com>
2022-08-20 07:12:10 -0600

    Allow warnings and errors to pass through

--- Server/noop-logger.ts
+++ Server/noop-logger.ts
@@ -3,10 +3,12 @@ export default class NoopLogger {
     info(_: string): void {
     }
 
-    warn(_: string): void {
+    warn(message: string): void {
+        console.warn(message);
     }
 
-    error(_: string): void {
+    error(message: string): void {
+        console.error(message);
     }
 
     debug(_: string): void {
